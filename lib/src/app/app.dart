fc3b570cde7b0358f93b869428123bbee1c30c9e
XxThunderBlastxX <koustavmondal55@gmail.com>
2023-07-23 20:01:34 +0530

    Made basic home UI

--- lib/src/app/app.dart
+++ lib/src/app/app.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'router/router.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ScreenUtilInit(
+      minTextAdapt: true,
+      builder: (context, _) => MaterialApp.router(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        routerDelegate: AppRouter().router.routerDelegate,
+        routeInformationParser: AppRouter().router.routeInformationParser,
+        routeInformationProvider: AppRouter().router.routeInformationProvider,
       ),
-      routerDelegate: AppRouter().router.routerDelegate,
-      routeInformationParser: AppRouter().router.routeInformationParser,
-      routeInformationProvider: AppRouter().router.routeInformationProvider,
     );
   }
 }
--- lib/src/feature/home/presentation/home_page.dart
+++ lib/src/feature/home/presentation/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
@@ -7,14 +8,71 @@ class HomePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: Center(
-        child: Text(
-          'Home Page',
-          style: Theme.of(context).textTheme.headline4,
-        ),
+      body: Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: 0.65.sh,
+            child: Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 0.40.sh,
+                  color: const Color(0xFFD90429),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 0.40.sh,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.transparent,
+                        const Color(0xFF000000).withOpacity(0.65),
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 0.08.sh,
+                  child: Text(
+                    'Flutter Dependency Adder',
+                    style: Theme.of(context).textTheme.headline6!.copyWith(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                ),
+                Positioned(
+                  top: 0.375.sh,
+                  child: SizedBox(
+                    width: 0.65.sw,
+                    height: 0.08.sh,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        hintText: 'Search for a package',
+                        contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 18,
+                          vertical: 8,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.r),
+                          borderSide: BorderSide(
+                            color: Colors.black87,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
