4d8a8891c0eee47ab929e5414eb5d1a1fdda16c0
XxThunderBlastxX <koustavmondal55@gmail.com>
2023-08-09 19:28:22 +0530

    Reduced package redudancy

--- lib/src/feature/search/provider/added_package_provider.dart
+++ lib/src/feature/search/provider/added_package_provider.dart
@@ -1,43 +1,43 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final addedPackageNotifierProvider =
-    StateNotifierProvider<AddedPackageNotifier, List<String>>(
+    StateNotifierProvider<AddedPackageNotifier, Set<String>>(
   (ref) => AddedPackageNotifier(),
 );
 
 final addedDevPackageNotifierProvider =
-    StateNotifierProvider<AddedDevPackageNotifier, List<String>>(
+    StateNotifierProvider<AddedDevPackageNotifier, Set<String>>(
   (ref) => AddedDevPackageNotifier(),
 );
 
-class AddedPackageNotifier extends StateNotifier<List<String>> {
-  AddedPackageNotifier() : super([]);
+class AddedPackageNotifier extends StateNotifier<Set<String>> {
+  AddedPackageNotifier() : super({});
 
   void addPackage(String package) {
-    state = [...state, package];
+    state = {...state, package};
   }
 
   void removePackage(String package) {
-    state = state.where((element) => element != package).toList();
+    state = state.where((element) => element != package).toSet();
   }
 
   void clear() {
-    state = [];
+    state = {};
   }
 }
 
-class AddedDevPackageNotifier extends StateNotifier<List<String>> {
-  AddedDevPackageNotifier() : super([]);
+class AddedDevPackageNotifier extends StateNotifier<Set<String>> {
+  AddedDevPackageNotifier() : super({});
 
   void addPackage(String package) {
-    state = [...state, package];
+    state = {...state, package};
   }
 
   void removePackage(String package) {
-    state = state.where((element) => element != package).toList();
+    state = state.where((element) => element != package).toSet();
   }
 
   void clear() {
-    state = [];
+    state = {};
   }
 }
